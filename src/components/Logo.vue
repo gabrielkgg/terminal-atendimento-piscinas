db0091034d1201b21f60743f792d6f539c30a676
Gabriel Alves <gabriel@meucrediario.com.br>
2024-06-23 10:27:55 -0300

    alterada logo e adicionado favicon

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,11 @@
 <template>
-	<img alt="Vue logo" src="../assets/logo.png" style="margin-bottom: 1em;"/>
+	<div>
+		<img alt="Vue logo" src="../assets/jck-logo.png" class="logo"/>
+	</div>
 </template>
+<style scoped>
+.logo {
+	max-width: 15em;
+	margin-bottom: 1em;
+}
+</style>
